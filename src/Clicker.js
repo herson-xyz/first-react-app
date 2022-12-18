b1dd3d90ab80e693aac0b9091b1f4d2e74176118
herson-xyz <watsrunner+github+herson@gmail.com>
2022-12-17 21:24:25 -0500

    save

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function Clicker()
 {
+    
+    const countState = useState(0)
+    const count = countState[0]
+    const setCount = countState[1]
+
+    const buttonClick = () =>
+    {
+        setCount(count + 1)
+    }
+
     return <div>
         <div>Clicks count: 0</div>
-        <button>Click Me</button>
+        <button onClick={ buttonClick }>Click Me</button>
     </div>
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // What is this doing? 
-// https://notes.herson.xyz/3+-+Studio+Notes/12/Documentation+-+First+React+App
+// https://notes.herson.xyz/3+-+Studio+Notes/12/Documentation+-+First+React+App+-+1
 //
 import { createRoot } from 'react-dom/client';
 import App from './App.js';

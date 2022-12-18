4254bf91c4557f1fc672b78f09ce776fb3158d79
herson-xyz <watsrunner+github+herson@gmail.com>
2022-12-17 21:45:25 -0500

    update

--- src/Clicker.js
+++ src/Clicker.js
@@ -3,17 +3,20 @@ import { useState } from "react";
 export default function Clicker()
 {
     
-    const countState = useState(0)
-    const count = countState[0]
-    const setCount = countState[1]
+    // const countState = useState(0) // A cleaner way to do this is to destructure. See example below:
+    // const count = countState[0]
+    // const setCount = countState[1]
+
+    const [ count, setCount] = useState(0)
 
     const buttonClick = () =>
     {
-        setCount(count + 1)
+        // setCount(value + 1) // this method doesn't work as well with async processes. We can get stale values. For better results, use method below.
+        setCount(value => value + 1) // simplified arrow function, (value) => { return value + 1 }
     }
 
     return <div>
-        <div>Clicks count: 0</div>
+        <div>Clicks count: { count }</div>
         <button onClick={ buttonClick }>Click Me</button>
     </div>
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // What is this doing? 
 // https://notes.herson.xyz/3+-+Studio+Notes/12/Documentation+-+First+React+App+-+1
-//
+
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import './style.css'

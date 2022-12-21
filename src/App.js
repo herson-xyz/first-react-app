ad29a9b56f20efb184c7723ea855cc6f9877a3bf
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2022-12-20 19:55:09 -0700

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import Clicker from "./Clicker.js";
 
 export default function App()
 {
+const [hasClicker, setHasClicker] = useState(true)                                                      // Simple way to think about it: If it's a variable that changes the app, it's a state variable
+
+    const toggleClickerClick = () =>
+    {
+        setHasClicker(!hasClicker)                                                                      // If it's true, return false. And vice versa.   
+    }
+
     return <>
-        <Clicker />
+        <button onClick={ toggleClickerClick }>{ hasClicker ? 'Hide' : 'Show'} Clicker</button>         {/*// Do not call the function. Provide the function. */}
+        {/* { hasClicker ? <Clicker /> : null} */}
+        { hasClicker && <Clicker /> }                                                                   {/* This is a condensed way of doing the same as the previous line*/}
     </>
 }

0271867f840ffe0ad1016433085ab800bec49ec3
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2022-12-29 14:05:10 -0500

    React - Update People.js to show the asynchronous way of making the API call

--- src/People.js
+++ src/People.js
@@ -9,11 +9,11 @@ export default function People()
         { id: 4, name: 'Boy' }
     ])
 
-    const getPeople = () => 
+    const getPeople = async () => 
     {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(result => console.log(result))
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const result = await response.json()
+        console.log(result);
     }
 
     useEffect(() =>
